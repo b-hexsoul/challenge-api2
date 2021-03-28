f546af51f838223a2d729f5202aaa61a6f35a24a
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-03-28 13:36:50 -0700

    'Add htmlExtract in urlController'

---
+++ controller/urlController.js
@@ -0,0 +1,23 @@
+const HtmlPage = require("../models/HtmlPage");
+
+// @desc    Extract Title and Meta:Description from url provided - Url is recieved from req.body.url;
+// @route   POST /api/v1/extractHtml
+// @access  Public
+exports.htmlExtract = async (req, res, next) => {
+  // Check if URL exists, if it does then increases the count and will return data.
+  const entry = await HtmlPage.findOne({ url: req.body.url });
+  console.log("entry", entry);
+  if (!entry) {
+    req.new = true;
+    let newDoc = await HtmlPage.create({ url: req.body.url });
+    console.log("newDoc", newDoc);
+    let { _id: id, url, createdAt } = newDoc;
+    res.status(200).json({ id, url, createdAt });
+  }
+
+  if (entry) {
+    req.new = false;
+    let { _id: id, url, createdAt } = entry;
+    res.status(200).json({ id, url, createdAt });
+  }
+};
